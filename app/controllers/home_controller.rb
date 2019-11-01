dcc8ceef7088d468645d506890d8328fd81c7ef7
Roman Blondel <rblondel23@gmail.com>
2019-11-01 15:19:14 +0100

    home page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,23 @@
+class HomeController < ApplicationController
+  def index
+    
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
